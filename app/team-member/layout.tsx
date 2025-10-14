4b5086e9925f9af291f73d41083f5dce017a2b7f
Felipe Tourn <tournfelipe@gmail.com>
2025-10-14 14:37:01 -0300

    navbar items team member

--- app/team-member/layout.tsx
+++ app/team-member/layout.tsx
@@ -2,9 +2,9 @@
 import MainHeader, {NavItem} from '@/components/main-header-user-view';
 
 const teamMemberNavItems: NavItem[] = [
-  { name: 'CALENDARIO', href: '/client/pilotos' },
-  { name: 'PUNTAJE', href: '/client/escuderias' },
-  { name: 'SANCIONES', href: '/carreras' },
+  { name: 'CALENDARIO', href: '/team-member/calendario' },
+  { name: 'PUNTAJE', href: '/team-member/puntaje' },
+  { name: 'SANCIONES', href: '/team-member/sanciones' },
 ];
 
 export default function TeamMemberLayout({ children }: { children: React.ReactNode }) {
