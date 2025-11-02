a346140554b9f67ced5012c45f4af77aba3acdc5
BonannoPablo <bonannopabloariel@gmail.com>
2025-11-02 20:35:06 -0300

    Agregada sanciones al layout de admin y cambiado el nombre de la pagina de perfiles a escuderias

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -3,10 +3,11 @@ import { ToastProvider } from "@/components/toast-provider";
 import MainHeader, {NavItem} from '../../components/main-header-user-view';
 
   const adminNavItems: NavItem[] = [
-  { name: 'PERFILES', href: '/admin/perfiles' },
+  { name: 'ESCUDERIAS', href: '/admin/escuderias' },
   { name: 'PILOTOS', href: '/admin/pilotos' },
   { name: 'PUNTOS', href: '/admin/puntos' },
   { name: 'EVENTOS', href: '/admin/eventos' },
+  { name: 'SANCIONES', href: '/admin/sanciones' },
 ];
 
 export default function AdminLayout({ children }: Readonly<{ children: React.ReactNode; }>) {
